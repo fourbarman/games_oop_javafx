c3a762784527410ad1a7bfbe3bb4cc4371310610
fourbarman <maks.java@yandex.ru>
2020-01-29 14:33:00 +0400

    Logic.move() test falling down

--- chess/src/test/java/ru/job4j/LogicTest.java
+++ chess/src/test/java/ru/job4j/LogicTest.java
@@ -1,4 +1,22 @@
 package ru.job4j;
 
+import org.junit.Test;
+import ru.job4j.chess.Logic;
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
+import ru.job4j.chess.firuges.black.BishopBlack;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 public class LogicTest {
+    @Test
+    public void whenTheWayIsClear() {
+        Logic logic = new Logic();
+        Figure bb = new BishopBlack(Cell.C8);
+        logic.add(bb);
+        boolean rst = logic.move(Cell.C8, Cell.H3);
+        rst = false;
+        //assertThat(rst, is(true));
+        assertThat(bb.position(), is(Cell.H3));
+    }
 }
